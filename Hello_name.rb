f776b98126a4e36c02fb7a7ad58d80e1e2d97510
Ann <jana.polya@yandex.ru>
2024-09-10 13:26:35 +0300

    сделан номер 3

--- Hello_name.rb
+++ Hello_name.rb
@@ -1,16 +1,49 @@
+#Получаем имя пользователя
 puts "Как вас зовут?"
-name=gets.chomp() #Получаем имя пользователя
+name=gets.chomp
 puts ("привет %s" %name)
+
+#Получаем язык, которым пользуется пользователь 
 puts ("какой язык любимый, %s?" %name)
-language=gets.chomp() #Получаем язык, которым пользуется пользователь 
+program_language=gets.chomp 
+
 # При ответе на вопрос какой язык любимый, %s?, срабатает конструкция 
 #if и выскажет своё мнение на счёт языка
-if language=="Ruby"
+if program_language=="Ruby"
   puts ("%s-подлиза" %name)
-elsif language=="c++"
+elsif program_language=="c++"
   puts "Интересный выбор. Но скоро будет ruby"
-elsif language=="c#"
+elsif program_language=="c#"
   puts "Странный язык. НО скоро будет ruby"
 else 
   puts "скоро будет ruby"
+end
+puts "%s, введите команду языка ruby" %name
+ruby_command = ""
+
+# запись всех команд которые будут введены, в переменную ruby_command
+while (line = gets.chomp) != 'end'
+  ruby_command += line + "\n"
+end
+
+# Выполнение команды Ruby
+begin
+  eval(ruby_command)
+rescue StandardError => error 
+  puts "Ошибка выполнения команды Ruby: #{error.message}"
+end
+
+puts "%s, введите команду OC" % name
+oc_command = ""
+
+# запись всех команд которые будут введены, в переменную oc_command
+while (line = gets.chomp) != 'end'
+  oc_command += line + "\n"
+end
+
+# Выполнение команды ОС
+begin
+  puts `#{oc_command}`
+rescue StandardError => error 
+  puts "Ошибка выполнения команды Ruby: #{error.message}"
 end
