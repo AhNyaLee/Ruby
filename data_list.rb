ad51ddcb1b0d2dba8e384c1c3b7ca1b6a8e48999
AhNyaLee <jana.polya@yandex.ru>
2024-12-13 14:24:53 +0300

    4laba 1.4.b number | created method get_select

--- data_list.rb
+++ data_list.rb
@@ -6,6 +6,12 @@ class Data_list
   def select(index)
     self.selected_elems = list[index]
   end
+  
+  def get_select(index)
+    self.selected_elems = list[index]
+    raise "Index out of range" if index < 0 || index >= list.length
+    self.selected_indexes << index
+  end
 
   private
   attr_accessor :list,  :selected_elems
